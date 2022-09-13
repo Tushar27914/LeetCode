85631277579f33e0a1226c91f1ad1c6e6746fa5b
Tushar27914 <112587369+Tushar27914@users.noreply.github.com>
2022-09-13 22:50:21 +0530

    Time: 76 ms (23.27%), Space: 13.9 MB (59.68%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -3,14 +3,29 @@ class Solution:
         c=0
         stack=[]
         for i in range(len(s)):
-            if(s[i]=="(" and c!=0):
+            if(s[i]=='(' and c!=0):
                 stack.append(s[i])
                 c+=1
-            elif(s[i]==")" and c!=1):
+            elif(s[i]==')' and c!=1):
                 stack.append(s[i])
                 c-=1
-            elif(s[i]=="(" and c==0):
+            elif(s[i]=='(' and c==0):
                 c+=1
             else:
                 c-=1
         return "".join(stack)
+                
+        # c=0
+        # stack=[]
+        # for i in range(len(s)):
+        #     if(s[i]=="(" and c!=0):
+        #         stack.append(s[i])
+        #         c+=1
+        #     elif(s[i]==")" and c!=1):
+        #         stack.append(s[i])
+        #         c-=1
+        #     elif(s[i]=="(" and c==0):
+        #         c+=1
+        #     else:
+        #         c-=1
+        # return "".join(stack)
