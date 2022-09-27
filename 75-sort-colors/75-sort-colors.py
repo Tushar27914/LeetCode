2e19bbc6a6193f4a55bb1e7df437dea5eb6da84e
Tushar27914 <112587369+Tushar27914@users.noreply.github.com>
2022-09-27 16:41:48 +0530

    Time: 66 ms (28.50%), Space: 14 MB (15.80%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        
+        nums.sort()
+        print(nums)
+        
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
