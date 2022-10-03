f0a1ad6c966d6519ebeb70c07e1e5de68d77bf55
Tushar27914 <112587369+Tushar27914@users.noreply.github.com>
2022-10-03 22:50:54 +0530

    Time: 116 ms (56.04%), Space: 14.1 MB (9.90%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -10,27 +10,27 @@ class Solution:
         carry=0
         while l1 or l2 or carry:
             if l1:
-                v1=l1.val
+                v1=l1.val #l1 value put the v1 variable
             else:
                 v1=0
             if l2:
-                v2=l2.val
+                v2=l2.val #l2 value put the v2 variable
             else:
                 v2=0
-            val=v1+v2+carry
-            carry=val//10
-            val=val%10
-            curr.next=ListNode(val)
-            curr=curr.next
+            val=v1+v2+carry #both v1 and v2 or carry sum put the val variable
+            carry=val//10  #find the carry
+            val=val%10  # find the value
+            curr.next=ListNode(val)  #put the value of the node
+            curr=curr.next #point to the next node
             if l1:
-                l1=l1.next
+                l1=l1.next #if l1 given poin to the next pointer
             else:
                 l1=None
             if l2:
-                l2=l2.next
+                l2=l2.next #if l2 given point to the next pointer
             else:
                 l2=None
-        return dummy.next
+        return dummy.next #return to the list
                 
                 
                 
