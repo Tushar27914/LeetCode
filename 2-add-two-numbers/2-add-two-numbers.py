a06d9dab64c2fc354920d238780e9edd4c6118a4
Tushar27914 <112587369+Tushar27914@users.noreply.github.com>
2022-10-04 15:37:01 +0530

    Time: 177 ms (5.25%), Space: 13.9 MB (85.93%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -10,27 +10,55 @@ class Solution:
         carry=0
         while l1 or l2 or carry:
             if l1:
-                v1=l1.val #l1 value put the v1 variable
+                v1=l1.val
             else:
                 v1=0
             if l2:
-                v2=l2.val #l2 value put the v2 variable
+                v2=l2.val
             else:
                 v2=0
-            val=v1+v2+carry #both v1 and v2 or carry sum put the val variable
-            carry=val//10  #find the carry
-            val=val%10  # find the value
-            curr.next=ListNode(val)  #put the value of the node
-            curr=curr.next #point to the next node
+            val=v1+v2+carry
+            carry=val//10
+            val=val%10
+            curr.next=ListNode(val)
+            curr=curr.next
+            
             if l1:
-                l1=l1.next #if l1 given poin to the next pointer
+                l1=l1.next
             else:
-                l1=None
+                l1=0
             if l2:
-                l2=l2.next #if l2 given point to the next pointer
+                l2=l2.next
             else:
-                l2=None
-        return dummy.next #return to the list
+                l2=0
+        return dummy.next
+            
+#         dummy=ListNode()
+#         curr=dummy
+#         carry=0
+#         while l1 or l2 or carry:
+#             if l1:
+#                 v1=l1.val #l1 value put the v1 variable
+#             else:
+#                 v1=0
+#             if l2:
+#                 v2=l2.val #l2 value put the v2 variable
+#             else:
+#                 v2=0
+#             val=v1+v2+carry #both v1 and v2 or carry sum put the val variable
+#             carry=val//10  #find the carry
+#             val=val%10  # find the value
+#             curr.next=ListNode(val)  #put the value of the node
+#             curr=curr.next #point to the next node
+#             if l1:
+#                 l1=l1.next #if l1 given poin to the next pointer
+#             else:
+#                 l1=None
+#             if l2:
+#                 l2=l2.next #if l2 given point to the next pointer
+#             else:
+#                 l2=None
+#         return dummy.next #return to the list
                 
                 
                 
