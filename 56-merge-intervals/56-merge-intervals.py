fd872135864b6eb3084cff87036d7e238d3d572d
Tushar27914 <112587369+Tushar27914@users.noreply.github.com>
2022-10-09 02:09:56 +0530

    Time: 343 ms (26.05%), Space: 18.1 MB (53.18%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -4,12 +4,21 @@ class Solution:
         intervals.sort(key=lambda x:x[0])
         stack.append(intervals[0])
         for i in range(1,len(intervals)):
-            
-            if intervals[i][0]<=stack[-1][1]:
+            if(intervals[i][0]<=stack[-1][1]):
                 a=stack.pop(-1)
                 stack.append([a[0],max(a[1],intervals[i][1])])
             else:
                 stack.append(intervals[i])
         return stack
+        # stack=[]
+        # intervals.sort(key=lambda x:x[0])
+        # stack.append(intervals[0])
+        # for i in range(1,len(intervals)):
+        #     if intervals[i][0]<=stack[-1][1]:
+        #         a=stack.pop(-1)
+        #         stack.append([a[0],max(a[1],intervals[i][1])])
+        #     else:
+        #         stack.append(intervals[i])
+        # return stack
         
         
