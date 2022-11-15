7792e5e22f267a2e34e37cfa9930bdd57bb23d24
Tushar27914 <112587369+Tushar27914@users.noreply.github.com>
2022-11-15 12:00:13 +0530

    Time: 60 ms (31.70%), Space: 13.9 MB (14.10%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.py
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.py
@@ -6,25 +6,40 @@
 #         self.right = right
 class Solution:
     def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
-        s=[]
-        if root is None:
-            return 0
-        q=deque()
-        q.append(root)
-        q.append(root)
-        while len(q)!=0:
-            level_size=len(q)
-            for i in range(level_size):
-                ce=q.popleft()
-                s.append(ce.val)
-                if ce.left is not None:
-                    q.append(ce.left)
-                if ce.right is not None:
-                    q.append(ce.right)
-        s=list(set(s))
-        if len(s)==1:
+        l=[]
+        def inorder(root):
+            if root:
+                inorder(root.left)
+                l.append(root.val)
+                inorder(root.right)
+        inorder(root)
+        print(l)
+        l=list(set(l))
+        if len(l)==1:
             return -1
-        s.sort()
-        return s[1]
+        l.sort()
+        return l[1]
+                
+            
+        # s=[]
+        # if root is None:
+        #     return 0
+        # q=deque()
+        # q.append(root)
+        # q.append(root)
+        # while len(q)!=0:
+        #     level_size=len(q)
+        #     for i in range(level_size):
+        #         ce=q.popleft()
+        #         s.append(ce.val)
+        #         if ce.left is not None:
+        #             q.append(ce.left)
+        #         if ce.right is not None:
+        #             q.append(ce.right)
+        # s=list(set(s))
+        # if len(s)==1:
+        #     return -1
+        # s.sort()
+        # return s[1]
             
         
