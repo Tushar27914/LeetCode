4d9c84b65ce68e201dac118ba32bb18c1eedd4bf
Tushar27914 <112587369+Tushar27914@users.noreply.github.com>
2022-11-15 12:16:25 +0530

    Time: 102 ms (81.58%), Space: 21.6 MB (11.61%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -7,18 +7,26 @@
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
         s=[]
-        if root is None:
-            return 0
-        q=deque()
-        q.append(root)
-        while len(q)!=0:
-            level_size=len(q)
-            for i in range(level_size):
-                ce=q.popleft()
-                s.append(ce.val)
-                if ce.left is not None:
-                    q.append(ce.left)
-                if ce.right is not None:
-                    q.append(ce.right)
+        def inorder(root):
+            if root :
+                inorder(root.left)
+                s.append(root.val)
+                inorder(root.right)
+        inorder(root)
         return len(s)
+#         s=[]
+#         if root is None:
+#             return 0
+#         q=deque()
+#         q.append(root)
+#         while len(q)!=0:
+#             level_size=len(q)
+#             for i in range(level_size):
+#                 ce=q.popleft()
+#                 s.append(ce.val)
+#                 if ce.left is not None:
+#                     q.append(ce.left)
+#                 if ce.right is not None:
+#                     q.append(ce.right)
+#         return len(s)
         
