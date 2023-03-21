25210fc00c2fd7a5d66c9d8536b1ffbc4abf06ea
Tushar27914 <112587369+Tushar27914@users.noreply.github.com>
2023-03-21 22:56:25 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -4,20 +4,36 @@ class Solution:
     
     #Function to return max value that can be put in knapsack of capacity W.
     def knapSack(self,W, wt, val, n):
-        def solver(val,wt,w,n,dp):
-            if dp[w][n]!=-1:
-                return dp[w][n]
-            if w==0 or n==0:
-                dp[w][n] = 0
-            elif w>=wt[n-1]:
-                dp[w][n] = max(val[n-1] + solver(val,wt,w-wt[n-1],n-1,dp),solver(val,wt,w,n-1,dp))
+        dp={}
+        def solver(N,W):
+            if N==0 or W==0:
+                return 0
+            elif (N,W) in dp:
+                return dp[(N,W)]
             else:
-                dp[w][n] = solver(val,wt,w,n-1,dp)
-            return dp[w][n]
+                if wt[N-1]<=W:
+                    c1=val[N-1]+solver(N-1,W-wt[N-1])
+                    c2=solver(N-1,W)
+                    c=max(c1,c2)
+                else:
+                    c=solver(N-1,W)
+                dp[(N,W)]=c
+                return c
+        return solver(n,W)
+        # def solver(val,wt,w,n,dp):
+        #     if dp[w][n]!=-1:
+        #         return dp[w][n]
+        #     if w==0 or n==0:
+        #         dp[w][n] = 0
+        #     elif w>=wt[n-1]:
+        #         dp[w][n] = max(val[n-1] + solver(val,wt,w-wt[n-1],n-1,dp),solver(val,wt,w,n-1,dp))
+        #     else:
+        #         dp[w][n] = solver(val,wt,w,n-1,dp)
+        #     return dp[w][n]
         
-        dp = [[-1]*(n+1) for _ in range(W+1)]
+        # dp = [[-1]*(n+1) for _ in range(W+1)]
         
-        return solver(val,wt,W,n,dp)
+        # return solver(val,wt,W,n,dp)
             
         # code here
 
